dbed7a53a6a6e8e86b18d580007767c377e700f6
SaiHar-shith <saiharshithkatanguri@gmail.com>
2025-12-16 01:03:41 +0530

    Connectivity issue fix

--- snippet-manager-backend/index.js
+++ snippet-manager-backend/index.js
@@ -1,3 +1,12 @@
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first'); 
+
+const express = require('express');
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first'); 
+
+const express = require('express');
+
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
